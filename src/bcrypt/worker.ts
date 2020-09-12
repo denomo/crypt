0576ebb5936a86fa2f37948d52b197b8559749c7
Bunlong <bunlong.van@gmail.com>
2020-09-12 14:15:39 +0700

    :heavy_plus_sign: switch hash

--- src/bcrypt/worker.ts
+++ src/bcrypt/worker.ts
@@ -3,5 +3,13 @@ import * as bcrypt from './mod.ts';
 const context: Worker = self as any;
 
 context.onmessage = (event) => {
-  
+  let data = event.data;
+  switch (data.action) {
+    case 'hash': {
+      context.postMessage(
+        bcrypt.hashPw(data.payload.plaintext, data.payload.salt)
+      );
+      break;
+    }
+  }
 };
